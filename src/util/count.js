623fcda0640c09d270329ea40d6ff2ea5ef690f9
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-09-02 23:45:42 -0400

    added some utils

---
+++ src/util/count.js
@@ -0,0 +1,11 @@
+
+module.exports = function (array, value, strict = true) {
+	let contains = 0;
+
+	for (let i = 0; i < array.length; i++) {
+		if (strict ? array[i] === value : array[i] == value)
+			contains++;
+	}
+
+	return contains;
+};
---
+++ src/util/validity.js
@@ -0,0 +1,7 @@
+
+module.exports = function (guess, size) {
+	if (guess.length !== size + 2)
+		return false;
+
+	return true;
+};
