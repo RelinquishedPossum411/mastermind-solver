9a9c92095581c89ce9e7370d143f747add4657db
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-09-05 16:44:33 -0400

    add util method containsAtIndex

---
+++ src/util/containsAtIndex.js
@@ -0,0 +1,15 @@
+
+module.exports = function containsAtIndex(array, value, index) {
+	let ind = array.indexOf(value);
+
+	if (ind > index)
+		return false;
+
+	if (ind === index)
+		return true;
+
+	if (ind !== -1)
+		return containsAtIndex(array.slice(ind + 1), value, index - ind - 1);
+
+	return false;
+};
