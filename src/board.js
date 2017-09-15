fbd9f76f60d4ba67095e3c0bffdd26c5e60e3af0
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-09-15 19:14:35 -0400

    Add wrapper containing set of pegs and keys

---
+++ src/board.js
@@ -0,0 +1,10 @@
+
+module.exports = class {
+	constructor(sequence, keys) {
+		const board = sequence;
+		const keysArray = keys;
+
+		this.getBoard = function () { return board; };
+		this.getKeys  = function () { return keysArray; };
+	}
+};
